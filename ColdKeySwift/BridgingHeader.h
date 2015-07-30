066dbaa584e200fae4286c9603c3ab4a3c69dda6
Huang Yu <huang@bitgo.com>
2015-07-29 18:57:48 -0700

    add new files

---
+++ ColdKeySwift/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  ColdKeySwift
+//
+//  Created by Huang Yu on 7/29/15.
+//  Copyright (c) 2015 BitGo, Inc. All rights reserved.
+//
+
+#ifndef ColdKeySwift_BridgingHeader_h
+#define ColdKeySwift_BridgingHeader_h
+
+
+#endif
