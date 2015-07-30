d39874128b573ba3b61f53172476686a35f07694
Huang Yu <huang@bitgo.com>
2015-07-30 03:24:50 -0700

    add new files

---
+++ ColdKeySwift/ColdKeySwift/QRCodeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QRCodeViewController.swift
+//  ColdKeySwift
+//
+//  Created by Huang Yu on 7/29/15.
+//  Copyright (c) 2015 BitGo, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class QRCodeViewController: NSObject {
+
+}
